8e738287370cceb712260b22944bc48649ab40ae
Sharon Kendi <sharonkendi35@gmail.com>
2025-03-12 13:30:37 +0300

    Uses array map method

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(" ") // Split the string into words
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize first letter
+      .join(" "); // Join words back into a string
+  });
 }
+
+console.log(titleCased());
